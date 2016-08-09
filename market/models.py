e61f8522a008bfa720fb8c7b285a4ddeb65b1227
Christine710 <christine.oeveren@gmail.com>
2016-08-09 23:27:58 +0200

    css, base.html en bootstrap toegevoegd

--- market/models.py
+++ market/models.py
@@ -1,5 +1,17 @@
-from __future__ import unicode_literals
-
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Product(models.Model):
+    product_type = models.CharField(max_length=200)
+    supplier = models.CharField(max_length=200)
+    quantity = models.CharField(max_length=10)
+    published_date = models.DateTimeField(
+        blank=True, null=True)
+    product_image = models.CharField(max_length=100)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.product_type + ' - ' + self.supplier
