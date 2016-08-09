445c6e134d12c2dae5ab805b8adb36dc8ac09978
Dirk-Philip <dirkphilip@gmail.com>
2016-08-09 18:06:32 +0200

    page navigatie toegevoegd

--- market/urls.py
+++ market/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^advertentie$', views.advertentie, name='advertentie'),
+    url(r'^register', views.register, name='register'),
 ]
 
--- market/views.py
+++ market/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'market/index.html', {})
+
+def advertentie(request):
+    return render(request, 'market/advertentie.html', {})
+
+def register(request):
+    return render(request, 'market/register.html', {})
